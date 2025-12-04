7cebce2e81793f254723bc63d4c58febde174b6e
tordrt <tordtafjord@gmail.com>
2025-12-04 22:04:07 +0700

    solutions day three

---
+++ 3/part-two.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	file, err := os.Open("3/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	totalOutput := 0
+	maxOutputStrBuilder := strings.Builder{}
+	maxOutputStrBuilder.Grow(12)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		bank := scanner.Text()
+
+		searchStart := 0
+		for digitsRemaining := 12; digitsRemaining > 0; digitsRemaining-- {
+			maxIdx := searchStart
+			maxByte := bank[searchStart]
+
+			for y := searchStart + 1; y <= len(bank)-digitsRemaining; y++ {
+				if bank[y] > maxByte {
+					maxByte = bank[y]
+					maxIdx = y
+				}
+			}
+
+			maxOutputStrBuilder.WriteByte(maxByte)
+			searchStart = maxIdx + 1
+		}
+
+		maxOutput, err := strconv.Atoi(maxOutputStrBuilder.String())
+		if err != nil {
+			log.Println("Failed to convert to int error:", err)
+			continue
+		}
+
+		maxOutputStrBuilder.Reset()
+		totalOutput += maxOutput
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	// Result
+	println(totalOutput)
+}
